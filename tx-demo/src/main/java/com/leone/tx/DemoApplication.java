e4b0cc37e22ef8b524f9667e6196394af03cf91f
leone <exklin@gmail.com>
2019-05-15 14:21:12 +0800

    init

---
+++ tx-demo/src/main/java/com/leone/tx/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.leone.tx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * @author leone
+ * @since 2019-05-14
+ **/
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
