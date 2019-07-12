6780efc8f7ffa7762be811ca597c0933fefc78e3
leone <exklin@gmail.com>
2019-07-12 15:02:07 +0800

    update limited

---
+++ microservice-limiting/src/main/java/com/leone/microservice/limiting/LimitedApplication.java
@@ -0,0 +1,19 @@
+package com.leone.microservice.limiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * <p>
+ *
+ * @author leone
+ * @since 2019-04-19
+ **/
+@EnableAspectJAutoProxy
+@SpringBootApplication
+public class LimitedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LimitedApplication.class, args);
+    }
+}
